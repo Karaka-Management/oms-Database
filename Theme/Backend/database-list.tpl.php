d2f3aaca98c70f296c67f38a0acaa925287b1abd
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-07-29 21:04:10 +0200

    adj. frontend uri handling of query/fragment

--- Theme/Backend/database-list.tpl.php
+++ Theme/Backend/database-list.tpl.php
@@ -25,26 +25,23 @@ $footerView->setPage(1);
 echo $this->getData('nav')->render(); ?>
 <div class="row">
     <div class="col-xs-12">
-        <div class="box wf-100">
+        <div class="portlet">
+            <div class="portlet-head"><?= $this->getHtml('Templates') ?><i class="fa fa-download floatRight download btn"></i></div>
             <table class="default">
-                <caption><?= $this->getHtml('Templates') ?><i class="fa fa-download floatRight download btn"></i></caption>
                 <thead>
                 <tr>
                     <td class="wf-100"><?= $this->getHtml('Name') ?>
                     <td><?= $this->getHtml('Creator') ?>
                     <td><?= $this->getHtml('Created') ?>
-                        <tfoot>
+                <tbody>
+                <?php $count = 0; foreach ($queries as $key => $value) : ++$count;
+                $url = \phpOMS\Uri\UriFactory::build('{/prefix}database/template/single?{?}&id=' . $value->getId()); ?>
                 <tr>
-                    <td colspan="3">
-                        <tbody>
-                        <?php $count = 0; foreach ($queries as $key => $value) : ++$count;
-                        $url = \phpOMS\Uri\UriFactory::build('{/prefix}database/template/single?{?}&id=' . $value->getId()); ?>
-                        <tr>
-                            <td><a href="<?= $url; ?>"><?= $this->printHtml($value->getName()); ?></a>
-                            <td><a href="<?= $url; ?>"><?= $this->printHtml($value->getCreatedBy()->getId()); ?></a>
-                            <td><a href="<?= $url; ?>"><?= $this->printHtml($value->getCreatedAt()->format('Y-m-d H:i:s')); ?></a>
-                        <?php endforeach; ?>
-                        <?php if ($count === 0) : ?>
+                    <td><a href="<?= $url; ?>"><?= $this->printHtml($value->getName()); ?></a>
+                    <td><a href="<?= $url; ?>"><?= $this->printHtml($value->getCreatedBy()->getId()); ?></a>
+                    <td><a href="<?= $url; ?>"><?= $this->printHtml($value->getCreatedAt()->format('Y-m-d H:i:s')); ?></a>
+                <?php endforeach; ?>
+                <?php if ($count === 0) : ?>
                 <tr><td colspan="5" class="empty"><?= $this->getHtml('Empty', '0', '0'); ?>
                         <?php endif; ?>
             </table>
