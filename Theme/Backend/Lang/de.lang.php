7d3ecb75c102339e6bee3936777950d3bc4ae741
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-02-08 19:10:15 +0100

    test language/export/import which changed a lot of lang files

---
+++ Theme/Backend/Lang/de.lang.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 8.0
+ *
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+return ['Database' => [
+    'Account' => '',
+    'Accounts' => '',
+    'Calculation' => '',
+    'Created' => '',
+    'Creator' => '',
+    'Database' => '',
+    'Dataformat' => '',
+    'Elements' => '',
+    'Fixdata' => '',
+    'General' => '',
+    'Group' => '',
+    'Groups' => '',
+    'Headlines' => '',
+    'Module' => '',
+    'Modules' => '',
+    'Name' => '',
+    'Options' => '',
+    'Permission' => '',
+    'Permissions' => '',
+    'Query' => '',
+    'Result' => '',
+    'Template' => '',
+    'Templates' => '',
+    'Status' => '',
+    'Next' => '',
+]];
--- Theme/Backend/Lang/en.lang.php
+++ Theme/Backend/Lang/en.lang.php
@@ -4,7 +4,6 @@
  *
  * PHP Version 8.0
  *
- * @package   Modules\Database
  * @copyright Dennis Eichhorn
  * @license   OMS License 1.0
  * @version   1.0.0
@@ -13,27 +12,29 @@
 declare(strict_types=1);
 
 return ['Database' => [
-    'Account'     => 'Account',
-    'Accounts'    => 'Accounts',
+    'Account' => 'Account',
+    'Accounts' => 'Accounts',
     'Calculation' => 'Calculation',
-    'Created'     => 'Created',
-    'Creator'     => 'Creator',
-    'Database'    => 'Database',
-    'Dataformat'  => 'Dataformat',
-    'Elements'    => 'Elements',
-    'Fixdata'     => 'Fixdata',
-    'General'     => 'General',
-    'Group'       => 'Group',
-    'Groups'      => 'Groups',
-    'Headlines'   => 'Headlines',
-    'Module'      => 'Module',
-    'Modules'     => 'Modules',
-    'Name'        => 'Name',
-    'Options'     => 'Options',
-    'Permission'  => 'Permission',
+    'Created' => 'Created',
+    'Creator' => 'Creator',
+    'Database' => 'Database',
+    'Dataformat' => 'Dataformat',
+    'Elements' => 'Elements',
+    'Fixdata' => 'Fixdata',
+    'General' => 'General',
+    'Group' => 'Group',
+    'Groups' => 'Groups',
+    'Headlines' => 'Headlines',
+    'Module' => 'Module',
+    'Modules' => 'Modules',
+    'Name' => 'Name',
+    'Options' => 'Options',
+    'Permission' => 'Permission',
     'Permissions' => 'Permissions',
-    'Query'       => 'Query',
-    'Result'      => 'Result',
-    'Template'    => 'Template',
-    'Templates'   => 'Templates',
+    'Query' => 'Query',
+    'Result' => 'Result',
+    'Template' => 'Template',
+    'Templates' => 'Templates',
+    'Status' => '',
+    'Next' => '',
 ]];
