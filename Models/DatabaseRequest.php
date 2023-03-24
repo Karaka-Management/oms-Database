4c7eeffe6448b9aa35bb412998d401425308df43
Dennis Eichhorn <spl1nes.com@googlemail.com>
2023-03-24 16:20:21 +0100

    registration fixes

--- Models/DatabaseRequest.php
+++ Models/DatabaseRequest.php
@@ -6,7 +6,7 @@
  *
  * @package   Modules\Database\Models
  * @copyright Dennis Eichhorn
- * @license   OMS License 1.0
+ * @license   OMS License 2.0
  * @version   1.0.0
  * @link      https://jingga.app
  */
@@ -18,7 +18,7 @@ namespace Modules\Database\Models;
  * Database request.
  *
  * @package Modules\Database\Models
- * @license OMS License 1.0
+ * @license OMS License 2.0
  * @link    https://jingga.app
  * @since   1.0.0
  */
